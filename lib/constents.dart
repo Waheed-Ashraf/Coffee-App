c8613368c88e4bff3cc73b15a38a236c21ed2f32
waheed_ashrf <washraf124@gmail.com>
2024-01-28 06:21:03 +0200

    first commit

---
+++ lib/constents.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete {
+  static Color backgroundColor = const Color(0xffFFDBAF);
+  static Color mainColor = const Color(0xff65451F);
+  static Color buttonColor = const Color(0xffCEE6F3);
+}
---
+++ lib/core/asset_data.dart
@@ -0,0 +1,6 @@
+class AssetsData {
+  static String onBoardingImage1 = 'assets/images/one.jpg';
+  static String onBoardingImage2 = 'assets/images/tow.jpg';
+  static String onBoardingImage3 = "assets/images/three.jpg";
+  static String onBoardingImage4 = "assets/images/four.jpg";
+}
